2f2bc3ec4865252908fb21ff6d97d7c1f9fce4a1
thalesleite <thalesaleite@gmail.com>
2020-06-24 11:51:23 +0100

    studying conversions

---
+++ chapter2/2-datatypes-and-sizes/basic.c
@@ -0,0 +1,11 @@
+// Basic data types in C:
+
+/*
+char:     a single byte, capable of holding one character in the local charracter set.
+int:      an integer, typically reflecting the natural size of integers on the host machine.
+float:    single-precision floating point.
+double:   double-precision floating point.
+
+short:    is often int 16bits.
+long:     is often int 32bits.
+*/
---
+++ chapter2/7-conversions/conversion.c
@@ -0,0 +1,20 @@
+// atoi: convert s to integer
+int atoi( char s[] ) {
+  int i, n;
+
+  n = 0;
+  for ( i = 0; s[i] >= '0' && s[i] <= '9'; ++1 ) {
+    n = 10 * n + ( s[i] - '0' );
+  }
+
+  return n;
+}
+
+// lower: convert c to lower case - ACSII only
+int lower( int c ) {
+  if ( c >= 'A' && c <= 'Z') {
+    return c + 'a' - 'A';
+  } else {
+    return c;
+  }
+}
